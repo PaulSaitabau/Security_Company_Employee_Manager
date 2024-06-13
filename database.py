4165d95882ed05c8e498fea3db5ba22df4b6e0da
paulsaitabau <paulsaitabau05@gmail.com>
2024-06-13 12:03:38 +0300

    additional classes

--- database.py
+++ database.py
@@ -2,3 +2,20 @@ from sqlalchemy import create_engine, Column, Integer, String, Date
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
+class OperationsGuard(Base):
+    __tablename__ = 'operations_guards'
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    start_date = Column(Date)
+    assignment = Column(String)
+    shift = Column(String)
+    location = Column(String)
+
+class ManagementGuard(Base):
+    __tablename__ = 'management_guards'
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    start_date = Column(Date)
+    assignment = Column(String)
+    shift = Column(String)
+    location = Column(String)
